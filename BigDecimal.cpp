f2d4197a09bfe1f1b3ade0b3fffc491de0a5b097
Djokjula <djordjemarjanovic998@gmail.com>
2017-12-12 21:17:59 +0100

    Added comments in the .cpp files

--- BigDecimal.cpp
+++ BigDecimal.cpp
@@ -33,6 +33,7 @@ BigDecimal::BigDecimal(const char *num) : mantis(num), sign(1) {
 	// update exponent after erasing zeros
 	exp -= last;
 
+	// if the mantis has no symbols left, it means the number passed is 0
 	if (mantis == "") {
 		exp = 1;
 		sign = 1;
@@ -45,178 +46,365 @@ BigDecimal::~BigDecimal() {
 }
 
 BigDecimal BigDecimal::add(const BigDecimal *b) const {
+
+	// helper pointer for this
 	const BigDecimal *a = this;
+
 	if (a->sign != b->sign) {
+
+		// if the signs are different, then a + b = a - (-b)
 		BigDecimal bneg = b->neg();
 		return a->sub(&bneg);
+
 	} else {
+
+		// first and last index for adding
 		long maxidx = MAX(a->maxIdx(), b->maxIdx());
 		long minidx = MIN(a->minIdx(), b->minIdx());
+
+		// stores the carry of a digit addition
 		int carry = 0;
+
+		// temporary return string
 		std::string ret_s = "";
+
+		// main adding loop
 		for (long i = minidx; i <= maxidx + 1; ++i) {
+
+			// add two digits and the carry (initially carry = 0)
 			int sum = a->numAt(i) + b->numAt(i) + carry;
+
+			// add the number to the last place of the string (storing it inverted)
 			ret_s.push_back('0' + sum % 10);
+
+			// calculate the carry for the next addition
 			carry = sum / 10;
 		}
+
+		// first guess of the exponent
 		long t_exp = maxidx;
+
 		while (t_exp < -1) {
+			// if the exponent is negative, add zeros until the decimal point
 			ret_s.push_back('0');
 			t_exp++;
 		}
+
+		// reverse the string, since it was stored inverted
 		std::reverse(ret_s.begin(), ret_s.end());
+
 		while (t_exp > static_cast<long>(ret_s.length() - 2)) {
+
+			// if the exponent is larger than the number of significant digits, add zeros until the decimal point
 			ret_s.push_back('0');
+
 		}
+
+		// add the decimal point
 		ret_s.insert(t_exp + 2, ".");
+
+		// add the negative sign if the number is positive
 		if (a->sign == -1) {
 			ret_s.insert(0, "-");
 		}
+
+		// return the BigDecimal formed from a char*
 		return BigDecimal(ret_s.c_str());
 	}
 }
 
 BigDecimal BigDecimal::sub(const BigDecimal *b) const {
+
+	// helper pointer for this
 	const BigDecimal *a = this;
+
 	if (a->sign != b->sign) {
+
+		// if the signs are different, then a + b = a - (-b)
 		BigDecimal bneg = b->neg();
 		return a->add(&bneg);
+
 	} else {
+
 		if (a->sign == 1) {
 			if (b->greater(a)) {
+				// if b > a >= 0, then return -(b - a)
 				BigDecimal ret = b->sub(a);
 				return ret.neg();
 			}
 		} else {
 			if (a->greater(b)) {
+				// if 0 > a > b, then return -(b - a)
 				BigDecimal ret = b->sub(a);
 				return ret.neg();
 			}
 		}
+
+		// first and last index for adding
 		long maxidx = MAX(a->maxIdx(), b->maxIdx());
 		long minidx = MIN(a->minIdx(), b->minIdx());
+
+		// stores the carry of a digit addition
 		int carry = 0;
+
+		// temporary return string
 		std::string ret_s = "";
+
+		// main subtracting loop
 		for (long i = minidx; i <= maxidx + 1; ++i) {
+
+			// subtracts two digits and the carry (initially carry = 0)
 			int sum = a->numAt(i) - b->numAt(i) + carry;
+
+			// add the number to the last place of the string (storing it inverted)
 			ret_s.push_back('0' + (sum + 10) % 10);
+
+			// calculate the carry for the next addition
 			carry = (sum - 9) / 10;
 		}
+
+		// first guess of the exponent
 		long t_exp = maxidx;
+
 		while (t_exp < -1) {
+
+			// if the exponent is negative, add zeros until the decimal point
 			ret_s.push_back('0');
 			t_exp++;
+
 		}
+
+		// reverse the string, since it was stored inverted
 		std::reverse(ret_s.begin(), ret_s.end());
+
 		while (t_exp > static_cast<long>(ret_s.length() - 2)) {
+
+			// if the exponent is larger than the number of significant digits, add zeros until the decimal point
 			ret_s.push_back('0');
+
 		}
+
+		// add the decimal point
 		ret_s.insert(t_exp + 2, ".");
+
+		// add the negative sign if the number is positive
 		if (a->sign == -1) {
 			ret_s.insert(0, "-");
 		}
+
+		// return the BigDecimal formed from a char*
 		return BigDecimal(ret_s.c_str());
 	}
 }
 
 BigDecimal BigDecimal::abs() const {
+
+	// temporary return value
 	BigDecimal ret(*this);
+
+	// remove the sign if it exists
 	ret.sign = 1;
+
+	// return the absolute value
 	return ret;
 }
 
 bool BigDecimal::greater(const BigDecimal *b) const {
+
 	if (sign != b->sign) {
+
+		// if a is positive and b is negative than a > b
 		return sign > b->sign;
+
 	} else if (exp != b->exp) {
+
+		// if the numbers have equal sign, the exponent changes the value
 		if (exp > b->exp) {
+
+			// if the exponent of a is larger then b, then a > b iff sign(a) == 1
 			return sign == 1;
+
 		} else {
+
+			// if the exponent of b is larger then a, then a > b iff sign(a) == -1
 			return sign == -1;
+
 		}
+
 	} else {
-		for (int i = maxIdx(); i >= MIN(minIdx(), b->minIdx()); --i) {
+
+		// find the first different digit
+		for (long i = maxIdx(); i >= MIN(minIdx(), b->minIdx()); --i) {
+
+			// if the numbers are different, the answer is known
 			if (numAt(i) > b->numAt(i)) {
+
+				// if the mantis of a is greater than b, then a > b iff sign(a) == 1
 				return sign == 1;
+
 			} else if (numAt(i) < b->numAt(i)) {
+
+				// if the mantis of b is greater than a, then a > b iff sign(a) == -1
 				return sign == -1;
+
 			}
+
 		}
+
 	}
+
+	// if the numbers are equal, one is not greater than the other
 	return false;
 }
 
 bool BigDecimal::less(const BigDecimal *b) const {
+
 	if (sign != b->sign) {
+
+		// if a is negative and b is positive than a < b
 		return sign < b->sign;
+
 	} else if (exp != b->exp) {
+
+		// if the numbers have equal sign, the exponent changes the value
 		if (exp < b->exp) {
+
+			// if the exponent of a is less then b, then a < b iff sign(a) == 1
 			return sign == 1;
+
 		} else {
+
+			// if the exponent of b is less then a, then a < b iff sign(a) == -1
 			return sign == -1;
+
 		}
+
 	} else {
-		for (int i = maxIdx(); i >= MIN(minIdx(), b->minIdx()); --i) {
+
+		// find the first different digit
+		for (long i = maxIdx(); i >= MIN(minIdx(), b->minIdx()); --i) {
+
+			// if the numbers are different, the answer is known
 			if (numAt(i) < b->numAt(i)) {
+
+				// if the mantis of a is less than b, then a < b iff sign(a) == 1
 				return sign == 1;
+
 			} else if (numAt(i) > b->numAt(i)) {
+
+				// if the mantis of b is less than a, then a < b iff sign(a) == -1
 				return sign == -1;
+
 			}
+
 		}
+
 	}
+
+	// if the numbers are equal, one is not less than the other
 	return false;
 }
 
 bool BigDecimal::equals(const BigDecimal *b) const {
+
+	// check if the values have the same sign and exponent
 	if (sign == b->sign && exp == b->exp) {
+
+		// find the first digit that differs, if it exists
 		for (int i = maxIdx(); i >= MIN(minIdx(), b->minIdx()); --i) {
+
+			// if there is a digit that differs, the numbers aren't equal
 			if (numAt(i) != b->numAt(i)) {
 				return false;
 			}
+
 		}
+
+		// if no different digits are found, the numbers are the same
 		return true;
-	} else return false;
+	} else {
+		// if the values differ by sign or exponent, return false
+		return false;
+	}
+
 }
 
 BigDecimal BigDecimal::shl(int n) const {
+
+	// temporary return value
 	BigDecimal ret(*this);
+
+	// move the exponent n times
 	ret.exp -= n;
+
+	// return the value with the moved exponent
 	return ret;
+
 }
 
 BigDecimal BigDecimal::shr(int n) const {
+
+	// temporary return value
 	BigDecimal ret(*this);
+
+	// move the exponent n times
 	ret.exp += n;
+
+	// return the value with the moved exponent
 	return ret;
+
 }
 
 BigDecimal BigDecimal::rmd(int *n) const {
+
+	// temporary return value
 	BigDecimal ret(*this);
+
+	// move the exponent to one past the last digit
 	ret.exp = mantis.length();
+
+	// return the value with the moved exponent
 	return ret;
+
 }
 
 BigDecimal::BigDecimal(const BigDecimal &bigDecimal) {
+
+	// copies all the fields
 	exp = bigDecimal.exp;
 	sign = bigDecimal.sign;
 	mantis = bigDecimal.mantis;
+
 }
 
 BigDecimal BigDecimal::neg() const {
+
+	// temporary return value
 	BigDecimal ret(*this);
+
+	// change the sign
 	ret.sign *= -1;
+
+	// return the value with the different sign
 	return ret;
+
 }
 
 std::ostream &operator<<(std::ostream &out, const BigDecimal &bigDecimal) {
+
+	// use the toString helper method for printing
 	out << bigDecimal.toString();
 	return out;
+
 }
 
 BigDecimal::BigDecimal() {
+
+	// default to 0
 	exp = 1;
 	sign = 1;
 	mantis = "0";
+
 }
 
 const BigDecimal BigDecimal::operator=(const BigDecimal &bigDecimal) {
--- RSearchTree.cpp
+++ RSearchTree.cpp
@@ -2,34 +2,68 @@
 #include <queue>
 
 RSearchTree::RSearchTree(std::vector<BigDecimal *> &values) {
+
+	// stack to emulate recursion
 	std::vector<StackElement> stack;
+
+	// accomulator array
 	std::vector<BigDecimal> acc;
+
+	// put the 0 as the first element
 	acc.push_back(BigDecimal("0"));
+
+	// calculate the array
 	for (auto &&v : values) {
 		acc.push_back(*acc.rbegin() + *v);
 	}
+
+	// initialize the root
 	root = new TreeNode(0, values.size() - 1, &acc[values.size()]);
+
+	// initialize stack
 	stack.push_back(StackElement(root, RIGHT, values.size() / 2, values.size() - 1));
 	stack.push_back(StackElement(root, LEFT, 0, values.size() / 2 - 1));
+
+	// main while loop
 	while (!stack.empty()) {
+
+		// current StackElement for processing
 		StackElement currSE = stack.back();
+
+		// pop the selected StackElement
 		stack.pop_back();
+
+		// temporary start and end for easier reading
 		int s = currSE.start;
 		int e = currSE.end;
+
+		// calculate the sum using the accomulation array
 		BigDecimal *sum = new BigDecimal(acc[e + 1] - acc[s]);
+
+		// create the temporary element
 		TreeNode *tNode = new TreeNode(s, e, sum);
+
+		// set the parent's child to the temporary node
 		if (currSE.side == RIGHT) {
 			currSE.parent->setRight(tNode);
 		} else {
 			currSE.parent->setLeft(tNode);
 		}
+
+		// size of the segment of the current node
 		int len = currSE.end - currSE.start;
+
+		// push for creating the element's child
 		if (len > 0) {
 			stack.push_back(StackElement(tNode, RIGHT, currSE.start + len / 2 + 1, currSE.end));
 			stack.push_back(StackElement(tNode, LEFT, currSE.start, currSE.start + len / 2));
 		}
+
+		// remove unused BigDecimal
 		delete sum;
+
 	}
+
 }
 
 RSearchTree::~RSearchTree() {
@@ -41,10 +75,20 @@ RSearchTree::RSearchTree(const RSearchTree &rSearchTree) {
 }
 
 RSearchTree &RSearchTree::operator=(const RSearchTree &rSearchTree) {
+
+	// no need for copy if the values are same
 	if (this != &rSearchTree) {
+
+		// free the past state
 		free_tree();
+
+		// a temporary stack for emulating recursion
 		std::vector<RSearchTree::StackElement> stack;
+
+		// create the root
 		root = new TreeNode(*rSearchTree.root);
+
+		// push the root's children if they exist
 		if (root->getRight() != nullptr) {
 			root->setRight(new TreeNode(*root->getRight()));
 			stack.push_back(RSearchTree::StackElement(root->getRight(), RIGHT, root->getRight()->getStart(),
@@ -55,9 +99,15 @@ RSearchTree &RSearchTree::operator=(const RSearchTree &rSearchTree) {
 			stack.push_back(RSearchTree::StackElement(root->getLeft(), LEFT, root->getLeft()->getStart(),
 													  root->getLeft()->getEnd()));
 		}
+
+		// main loop
 		while (!stack.empty()) {
+
+			// get the current element
 			RSearchTree::StackElement currSE = stack.back();
 			stack.pop_back();
+
+			// push the current element's children if they exist and copy them to this
 			if (currSE.parent->getRight() != nullptr) {
 				currSE.parent->setRight(new TreeNode(*currSE.parent->getRight()));
 				stack.push_back(RSearchTree::StackElement(currSE.parent->getRight(), RIGHT,
@@ -70,37 +120,62 @@ RSearchTree &RSearchTree::operator=(const RSearchTree &rSearchTree) {
 														  currSE.parent->getLeft()->getStart(),
 														  currSE.parent->getLeft()->getEnd()));
 			}
+
 		}
 	}
+
 	return *this;
 }
 
 std::vector<TreeNode *> RSearchTree::intervalNodes(int start, int end) const {
+
+	// temporary return and stack emulator
 	std::vector<TreeNode *> stack, ret;
 	stack.push_back(root);
+
+	// main loop
 	while (!stack.empty()) {
+
+		// get the current node
 		TreeNode *tNode = stack.back();
 		stack.pop_back();
+
 		if (tNode->covers(start, end)) {
+
+			// if the node is contained, add it to the return value
 			ret.push_back(tNode);
+
 		} else {
+
+			// add children if they exist
 			if (tNode->getRight() != nullptr) {
 				stack.push_back(tNode->getRight());
 			}
 			if (tNode->getLeft() != nullptr) {
 				stack.push_back(tNode->getLeft());
 			}
+
 		}
+
 	}
+
 	return ret;
 }
 
 BigDecimal RSearchTree::intervalSum(int start, int end) const {
+
+	// array to store the intervalNodes
 	auto arr = intervalNodes(start, end);
+
+	// initialize the sum
 	BigDecimal sum("0");
+
+	// loop through the nodes and add the elements
 	for (auto &item : arr) {
 		sum += item->getVal();
 	}
+
+	// return the sum
 	return sum;
 }
 
